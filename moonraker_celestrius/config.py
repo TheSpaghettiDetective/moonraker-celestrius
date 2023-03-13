f176435004110225ee6b5ec65f91ab0cf6a4aa6f
Kenneth Jiang <kenneth.jiang@gmail.com>
2023-03-13 15:25:10 -0700

    Edge case

--- moonraker_celestrius/config.py
+++ moonraker_celestrius/config.py
@@ -1,10 +1,16 @@
 import configparser
 import os
 import argparse
+import signal
+import sys
 
 CYAN='\033[0;96m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+
+def config_interrupted(signum, frame):
+    print('')
+    sys.exit(1)
 
 def configure(config_path):
 
@@ -19,10 +25,16 @@ def configure(config_path):
     print(CYAN + "Configuring the server info for your Moonraker server.\n" + NC)
     mr_host = config.get("moonraker", "host", fallback="127.0.0.1")
     mr_host = input(f"Moonraker IP address or hostname (current: {mr_host}): ") or mr_host
+    mr_host = mr_host.strip()
+    if not mr_host:
+        config_interrupted(None, None)
     config.set("moonraker", "host", mr_host)
 
     mr_port = config.get("moonraker", "port", fallback="7125")
     mr_port = input(f"Moonraker port (current: {mr_port}): ") or mr_port
+    mr_port = mr_port.strip()
+    if not mr_port:
+        config_interrupted(None, None)
     config.set("moonraker", "port", mr_port)
 
     if "nozzle_camera" not in config:
@@ -36,6 +48,10 @@ you configured for your printer. Be sure to enter the URL for the correct camera
 
     snapshot_url = config.get("nozzle_camera", "snapshot_url", fallback="")
     snapshot_url = input(f"Nozzle camera snapshot URL (current: {snapshot_url}): ") or snapshot_url
+    snapshot_url = snapshot_url.strip()
+    if not snapshot_url:
+        config_interrupted(None, None)
+
     config.set("nozzle_camera", "snapshot_url", snapshot_url)
 
     if "celestrius" not in config:
@@ -49,14 +65,20 @@ uploaded to the server.
 
     pilot_email = config.get("celestrius", "pilot_email", fallback="")
     pilot_email = input(f"The email you signed up for the pilot program with (current: {pilot_email}): ") or pilot_email
+    pilot_email = pilot_email.strip()
+    if not pilot_email:
+        config_interrupted(None, None)
+
     config.set("celestrius", "pilot_email", pilot_email)
 
     # Save the updated configuration to the config file
     with open(config_path, "w") as f:
         config.write(f)
 
-
 if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, config_interrupted)
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-c', '--config', required=True,
